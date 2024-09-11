0f9217192ad1be68cdbbe2f03eef7c9696fea7f0
David Santiago Rodriguez Moreno <davidsantiagorodriguez@hotmail.com>
2024-09-10 19:39:33 -0500

    [+] .git ignore [+] index.js: lanza servidor en puerto 3000 y crea 3 endpoints

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log( 'Bienvienidos a esta API' );
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const www = process.env.WWW || './';
+app.use(express.static(www));
+console.log(`serving ${www}`);
+
+/** Endpoints de nuestro servidor  */
+
+//http://localhost:3000
+app.get('/', (req, res) => {
+    res.sendFile(`index.html`, { root: www });
+});
+//http://localhost:3000/about-us
+app.get('/about-us', (req, res) => {
+    res.sendFile(`index.html`, { root: www });
+});
+//http://localhost:3000/services
+app.get('/services', (req, res) => {
+    res.sendFile(`index.html`, { root: www });
+});
+//Inciador del servidor
+app.listen(port, () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`));
